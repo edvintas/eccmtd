42f87d6b7d7353656b3724836317b34c56f68df4
Edvinas Bazaras <edvintas@gmail.com>
2024-11-21 12:45:41 +0200

    Initial commit

---
+++ eccmtd.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int eccmtd_init(void) {
+	printk(KERN_INFO "eccmtd loaded 123");
+	return 0;
+}
+
+static void __exit eccmtd_exit(void) {
+	printk(KERN_INFO "eccmtd unloaded 456");
+}
+
+module_init(eccmtd_init);
+module_exit(eccmtd_exit);
+
+MODULE_DESCRIPTION("Error correction module for MTD interface");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Edvinas Bazaras <edvintas@gmail.com>");

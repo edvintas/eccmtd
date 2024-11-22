28a6629a944de0d233153caea58a17e9527520b3
Edvinas Bazaras <edvintas@gmail.com>
2024-11-22 13:59:38 +0200

    Some sample code from https://github.com/mwarning/mtdRW

--- main.c
+++ main.c
@@ -144,6 +144,29 @@ static void sblkdev_exit(void)
 module_param_named(catalog, sblkdev_catalog, charp, 0644);
 MODULE_PARM_DESC(catalog, "New block devices catalog in format '<name>,<capacity sectors>;...'");
 
+static int mtdRW_init(void) {
+    struct mtd_info *mtd;
+    bool keep_going;
+    int n;
+
+    printk(KERN_INFO "mtdRW: Start to unlock MTD partitions!\n");
+
+    keep_going = true;
+    for (n = 0; keep_going; n++) {
+        mtd = get_mtd_device(NULL, n);
+    if (!IS_ERR(mtd)) {
+            mtd->flags |= MTD_WRITEABLE;
+            printk(KERN_INFO "mtdRW: Make mtd device writeable: %s\n", mtd->name);
+            put_mtd_device(mtd);
+        } else {
+            keep_going = false;
+        }
+    }
+
+   return 0;
+}
+
+
 static int __init eccmtd_init(void) {
         printk(KERN_INFO "eccm loaded 12344444");
 //      sblkdev_add(0, 0, NULL, NULL);
